ee8f63b871aaac28187f9719854b6c5aa9b2c9e2
shuhul <shuhulmujoo@gmail.com>
2021-08-29 18:27:12 -0700

    Fixed Point Class almost done

---
+++ TeamCode/src/main/java/util/Point.java
@@ -0,0 +1,16 @@
+package util;
+
+public class Point {
+    public double x;
+    public double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void translate(double x_shift, double y_shift){
+        //Finish this method
+    }
+
+}
